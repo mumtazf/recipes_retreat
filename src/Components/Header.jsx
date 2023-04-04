f7fca7f62674d02720f421442f366fa23bcfa079
Mumtaz <fatim25m@mtholyoke.edu>
2023-04-03 23:04:08 -0400

    updated img src again

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
 return (
 <div>
-    <img className = "icon" src = "public/static/recipe.png" alt = "logo"/>
+    <img className = "icon" src = "../public/static/recipe.png" alt = "logo"/>
     <h1 className = "header"> Recipes Retreat </h1>
 
 </div>
